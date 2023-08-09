a48c24c23ebe39b4d069f21ec0b9bd4c401db98e
bluesapling13 <bluesapling13@bluesapling13s-MacBook-Air.local>
2023-08-09 12:35:20 +0530

    foodlog final output

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,7 @@ class EntriesController < ApplicationController
 
   # GET /entries or /entries.json
   def index
-    @entries = Entry.all
+    @entries = Entry.where("created_at >= ?", Date.today)
   end
 
   # GET /entries/1 or /entries/1.json
@@ -18,7 +18,6 @@ class EntriesController < ApplicationController
   # GET /entries/1/edit
   def edit
   end
-
   # POST /entries or /entries.json
   def create
     @entry = Entry.new(entry_params)
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakfast",calories: 400 ,carbohydrates: 35, proteins: 20,fats:10)
+    assert entry.save
+  end
 end
